13423adee056edf0444ed443caa8c1f64398186a
Emery Berger <emery.berger@gmail.com>
2012-09-19 08:25:58 -0400

    Improved Makefile; added a test.

---
+++ examples/falsesharing.cpp
@@ -0,0 +1,45 @@
+#include <pthread.h>
+
+#include <iostream>
+using namespace std;
+
+enum { MAX_THREADS = 256 };
+enum { NUM_ITERATIONS = 80000000 };
+
+class Item {
+public:
+  volatile int x[MAX_THREADS];
+};
+
+Item * theItem;
+
+void * worker (void * v) {
+  long index = (long) v;
+  for (int i = 0; i < NUM_ITERATIONS; i++) {
+    theItem->x[index]++;
+  }
+  return NULL;
+}
+
+
+int
+main()
+{
+  theItem = new Item;
+
+  pthread_t thread[MAX_THREADS];
+
+  cout << "Starting threads." << endl;
+
+  for (int i = 0; i < 8; i++) {
+    pthread_create (&thread[i], NULL, worker, (void *) i);
+  }
+ 
+  for (int i = 0; i < 8; i++) {
+    pthread_join (thread[i], NULL);
+  }
+
+  cout << "Done." << endl;
+ 
+  return 0;
+}
