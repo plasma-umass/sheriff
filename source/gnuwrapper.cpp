5d87193979ec5c496e7998858e1031d03743f34f
Emery Berger <emery.berger@gmail.com>
2012-09-23 13:55:09 -0400

    Added MALLOC_HOOK_VOLATILE check.

--- source/gnuwrapper.cpp
+++ source/gnuwrapper.cpp
@@ -66,7 +66,11 @@ extern "C" {
   static void *(*old_realloc_hook)(void *ptr, size_t size, const void *caller);
   static void *(*old_memalign_hook)(size_t alignment, size_t size, const void *caller);
 
-  void (* volatile __malloc_initialize_hook) (void) = my_init_hook;
+#ifndef __MALLOC_HOOK_VOLATILE
+#define __MALLOC_HOOK_VOLATILE
+#endif
+
+  void (* __MALLOC_HOOK_VOLATILE __malloc_initialize_hook) (void) = my_init_hook;
 
   static void my_init_hook (void) {
     // Store the old hooks.
