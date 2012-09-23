d097966a4546b64a27f1febc42193f365ebf056e
Emery Berger <emery.berger@gmail.com>
2012-09-23 15:29:04 -0400

    Added.

---
+++ include/wordchangeinfo.h
@@ -0,0 +1,12 @@
+// -*- C++ -*-
+
+#ifndef SHERIFF_WORDCHANGEINFO_H
+#define SHERIFF_WORDCHANGEINFO_H
+
+class wordchangeinfo {
+public:
+  unsigned short tid;
+  unsigned short version;
+};
+
+#endif
